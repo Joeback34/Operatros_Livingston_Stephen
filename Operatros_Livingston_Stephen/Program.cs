397f4e4d1886b2a3b65725c682cc60037cdc7aa2
Joeback34 <103281091+Joeback34@users.noreply.github.com>
2022-11-29 08:55:20 -0600

    Update Program.cs

--- Operatros_Livingston_Stephen/Program.cs
+++ Operatros_Livingston_Stephen/Program.cs
@@ -6,7 +6,18 @@ namespace Operatros_Livingston_Stephen
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            int intMult = 6 * 45;
+            int intAdd = 55 + 66;
+
+            Console.WriteLine($"The max of {intMult} and {intAdd} is {Math.Max(intMult, intAdd)}." +
+                $"The min of two is {Math.Min(intMult, intAdd)}.");
+
+            bool isIntGreater = intMult > intAdd;
+            Console.WriteLine($"{intMult} is greater than {intAdd}, {isIntGreater}.");
+
+
+
         }
     }
 }
